b89bedb70ee81f2e038ab5d38e87e9862ae4c423
Jens de Ruiter <jensgamesnl@gmail.com>
2023-01-30 21:35:40 +0100

    Make chicken have no drops

--- src/main/java/me/moodyzoo/hraevent/hra/modifiers/ChickenBomb.java
+++ src/main/java/me/moodyzoo/hraevent/hra/modifiers/ChickenBomb.java
@@ -3,11 +3,11 @@ package me.moodyzoo.hraevent.hra.modifiers;
 import me.moodyzoo.hraevent.hra.Hra;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.entity.Chicken;
-import org.bukkit.entity.Egg;
-import org.bukkit.entity.EntityType;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 
 @Mod
 public class ChickenBomb extends BaseMod {
@@ -29,6 +29,7 @@ public class ChickenBomb extends BaseMod {
             Egg egg = (Egg) event.getEntity();
             Location loc = egg.getLocation();
             Chicken chicken = (Chicken) loc.getWorld().spawnEntity(loc, EntityType.CHICKEN);
+            chicken.setMetadata("noMobLoot", new FixedMetadataValue(Hra.plugin, true));
             //run after 5 seconds
             Bukkit.getScheduler().scheduleSyncDelayedTask(Hra.plugin, new Runnable() {
                 @Override
@@ -37,10 +38,14 @@ public class ChickenBomb extends BaseMod {
                     chicken.remove();
                 }
             }, 50L);
+        }
+    }
 
-
-
-
+    @EventHandler
+    public void onDeath(EntityDeathEvent event) {
+        Entity entity = event.getEntity();
+        if(event.getEntity() instanceof Chicken && entity.hasMetadata("noMobLoot")) {
+            event.getDrops().clear();
         }
     }
 
