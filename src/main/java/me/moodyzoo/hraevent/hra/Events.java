5d12023af5cdc9165823a3a33d8392be52111e91
Jens de Ruiter <jensgamesnl@gmail.com>
2023-02-01 16:15:19 +0100

    Move killchance from events to mod framework

--- src/main/java/me/moodyzoo/hraevent/hra/Events.java
+++ src/main/java/me/moodyzoo/hraevent/hra/Events.java
@@ -17,47 +17,6 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 
 public class Events implements Listener {
 
-
-    @EventHandler
-    public void onPlayerAttacked(EntityDamageByEntityEvent e) {
-        if(e.getEntity().getType() == EntityType.PLAYER) {
-            Player player = e.getDamager().getType() == EntityType.PLAYER ? (Player) e.getDamager() : null;
-            if(Hra.inGame) {
-                //if player not has permission hra.slaan
-                if(!player.hasPermission("hra.slaan")) {
-                    return;
-                }
-
-                //get player who hit
-
-                //get player who got hit
-                Player target = e.getEntity().getType() == EntityType.PLAYER ? (Player) e.getEntity() : null;
-
-                //kill them both
-
-                //50% chance to kill the player who hit
-                if(Math.random() > 0.5) {
-                    player.setHealth(0);
-                }
-                target.setHealth(0);
-            }
-
-            /*
-            //if player has permsission hra.cool
-            if(player.hasPermission("hra.cool")) {
-                //get player who got hit
-                Player target = e.getEntity().getType() == EntityType.PLAYER ? (Player) e.getEntity() : null;
-                //check if they are holding a item containing the word "koter"
-                if(player.getInventory().getItemInMainHand().getType() == Material.RED_CANDLE) {
-                    //kill them
-                    target.setHealth(0);
-                }
-            }
-
-             */
-        }
-    }
-
     @EventHandler
     public void onDie(PlayerDeathEvent e) {
         if(PlayerManager.getAlive().contains(e.getPlayer())) {
---
+++ src/main/java/me/moodyzoo/hraevent/hra/modifiers/KillChance.java
@@ -0,0 +1,71 @@
+package me.moodyzoo.hraevent.hra.modifiers;
+
+import me.moodyzoo.hraevent.hra.Hra;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+
+@RegisterMod
+public class KillChance extends BaseMod {
+
+    public static String getName() {
+        return "killkans";
+    }
+
+    public void enable() {
+        super.enable();
+        // enable dingen
+    }
+
+    @Override
+    public void disable() {
+        super.disable();
+        // disable dingen
+    }
+
+    @EventHandler
+    public void onPlayerAttacked(EntityDamageByEntityEvent e) {
+        if(e.getEntity().getType() == EntityType.PLAYER) {
+            Player player = e.getDamager().getType() == EntityType.PLAYER ? (Player) e.getDamager() : null;
+            if(Hra.inGame) {
+                //if player not has permission hra.slaan
+                if(!player.hasPermission("hra.slaan")) {
+                    return;
+                }
+
+                //get player who hit
+
+                //get player who got hit
+                Player target = e.getEntity().getType() == EntityType.PLAYER ? (Player) e.getEntity() : null;
+
+                //kill them both
+
+                //50% chance to kill the player who hit
+                if(Math.random() > 0.5) {
+                    player.setHealth(0);
+                }
+                target.setHealth(0);
+            }
+
+            /*
+            //if player has permsission hra.cool
+            if(player.hasPermission("hra.cool")) {
+                //get player who got hit
+                Player target = e.getEntity().getType() == EntityType.PLAYER ? (Player) e.getEntity() : null;
+                //check if they are holding a item containing the word "koter"
+                if(player.getInventory().getItemInMainHand().getType() == Material.RED_CANDLE) {
+                    //kill them
+                    target.setHealth(0);
+                }
+            }
+
+             */
+        }
+    }
+
+}
